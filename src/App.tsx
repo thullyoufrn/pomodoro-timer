ec8859c10c7e1736e4d26d497e32d661ba3a67ea
Thullyo Damasceno <thullyodamasceno.3@gmail.com>
2023-03-09 16:07:40 -0300

    react router added

--- src/App.tsx
+++ src/App.tsx
@@ -1,13 +1,16 @@
 import { ThemeProvider } from 'styled-components'
-import { GlobalStyle } from './styles/global'
+import { BrowserRouter } from 'react-router-dom'
+import { Router } from './Router'
 
+import { GlobalStyle } from './styles/global'
 import { defaultTheme } from './styles/themes/default'
 
 export function App() {
   return (
     <ThemeProvider theme={defaultTheme}>
-      <h1>Pomodoro Timer</h1>
-
+      <BrowserRouter>
+        <Router />
+      </BrowserRouter>
       <GlobalStyle />
     </ThemeProvider>
   )
