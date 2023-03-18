d04243964c0402991c186320e852c3fac7fc488b
Thullyo Damasceno <thullyodamasceno.3@gmail.com>
2023-03-18 13:17:13 -0300

    react-hook-form addition

--- src/pages/Home/index.tsx
+++ src/pages/Home/index.tsx
@@ -1,13 +1,53 @@
-import { Play } from "phosphor-react";
-import { CountdownContainer, FormContainer, HomeContainer, MinutesAmountInput, Separator, StartCountdownButton, TaskInput } from "./styles";
+import { Play } from 'phosphor-react'
+import { useForm } from 'react-hook-form'
+import { zodResolver } from '@hookform/resolvers/zod'
+import * as zod from 'zod'
+
+import { 
+  CountdownContainer, 
+  FormContainer, 
+  HomeContainer, 
+  MinutesAmountInput, 
+  Separator, 
+  StartCountdownButton, 
+  TaskInput 
+} from './styles'
+
 
 export function Home() {
+  const { register, handleSubmit, watch, reset } = useForm({
+    defaultValues: {
+      task: '',
+      minutesAmount: 0,
+    }
+  })
+
+  interface NewCycleFormData {
+    task: string
+    minutesAmount: number
+  }
+
+  function handleCreateNewCycle(data: NewCycleFormData) {
+    console.log(data)
+    
+    reset()
+  }
+
+  const task = watch('task')
+  const minutesAmount = watch('minutesAmount')
+  const isSubmitDisabled = !task || !minutesAmount
+
   return (
     <HomeContainer>
-      <form action="">
+      <form onSubmit={handleSubmit(handleCreateNewCycle)} action="">
         <FormContainer>
           <label htmlFor="task">I will work on</label>
-          <TaskInput id="task" list="task-suggestions" placeholder="Name your project" />
+          <TaskInput 
+            id="task" 
+            list="task-suggestions" 
+            placeholder="Name your project" 
+            {...register('task')}
+          />
 
           <datalist id="task-suggestions">
             <option value="Ignite Feed"></option>
@@ -23,6 +63,7 @@ export function Home() {
             step={5}
             min={5}
             max={60}
+            {...register('minutesAmount', { valueAsNumber: true })}
           />
 
           <span>minutes.</span>
@@ -36,7 +77,7 @@ export function Home() {
           <span>0</span>
         </CountdownContainer>
 
-        <StartCountdownButton disabled type="submit">
+        <StartCountdownButton disabled={isSubmitDisabled} type="submit">
           <Play size={24} />
           Start
         </StartCountdownButton>
